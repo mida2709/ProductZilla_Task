3f49bbf8893865c3c9b7ccf211527dc0309a1e5a
mida2709 <126259059+mida2709@users.noreply.github.com>
2023-03-15 19:32:23 +0700

    Add files via upload

---
+++ productzilla_D10_method_take_home_task/productzilla_D10_method_take_home_task/scrip.js
@@ -0,0 +1,199 @@
+//1
+let nama;
+function setNama(Mida){
+    nama = Mida 
+}
+function getNama (Mida){
+    return nama
+}
+console.log(getNama('Mida'));
+
+//2
+let pelajaran;
+function setPelajaran(Kimia){
+    pelajaran = Kimia
+}
+function getPelajaran (Kimia){
+    return pelajaran
+}
+console.log(getPelajaran('Kimia'));
+
+//3
+let Agama;
+function setAgama(Kristen){
+    Agama = Kristen
+}
+function getAgama (Kristen){
+    return Agama
+}
+console.log(getAGama('Kristen'));
+
+//4
+let Makanan;
+function setMakanan(Nasi){
+    Makanan = nasi
+}
+function getMakanan (Nasi){
+    return Makanan
+}
+console.log(getMakanan(`Nasi`));
+
+//5
+let Minuman;
+function setMinuman(Fanta){
+    Minuman = Fanta
+}
+function getMinuman(Fanta){
+    return Minuman
+}
+console.log(getMinuman(`Fanta`));
+
+//6
+let Warna1;
+function setWarna1(Merah){
+    Warna1 = Merah
+}
+function getWarna1(Merah){
+    return Warna1
+}
+console.log(getWarna1(Merah));
+
+//7
+let Warna2;
+function setWarna2(Hijau){
+    Warna2 = Hijau
+}
+function getWarna2(Hijau){
+    return Warna2
+}
+console.log(getWarna2(Hijau));
+
+//8
+let Warna3;
+function setWarna2(Biru){
+    Warna3 = Biru
+}
+function getWarna2(Biru){
+    return Warna3
+}
+console.log(getWarna3(Biru));
+
+//9
+let Bunga1;
+function setBunga1(Mawar){
+    Bunga1 = Biru
+}
+function getBunga1(Mawar){
+    return Bunga1
+}
+console.log(getBunga1(Mawar));
+
+//10
+let Bunga2;
+function setBunga2(Melati){
+    Bunga2 = Melati
+}
+function getBunga2(Melati){
+    return Bunga2
+}
+console.log(getBunga2(Melati));
+
+//11
+let Bunga3;
+function setBunga3(Anggrek){
+    Bunga3 = Anggrek
+}
+function getBunga3(Anggrek){
+    return Bunga3
+}
+console.log(getBunga3(Anggrek));
+
+//12
+let Hobi;
+function setHobi(Memasak){
+    Hobi= Memasak
+}
+function getHobi(Memasak){
+    return Hobi
+}
+console.log(getHobi(Memasak));
+
+//13
+let Nama1;
+function setNama1(crist){
+    Nama1 = crist
+}
+function getNama1(crist){
+    return Nama1
+}
+console.log(getNama1(crist));
+
+//14
+let Nama2;
+function setNama2(Manto){
+    Nama2 = Manto
+}
+function getNama2(Manto){
+    return Nama2
+}
+console.log(getNama2(Manto));
+
+//15
+let pelajaran1;
+function setPelajaran1(Fisika){
+    pelajaran1 = Fisika
+}
+function getPelajaran1(Fisika){
+    return pelajaran1
+}
+console.log(getPelajaran1('Fisika'));
+
+//16
+let pelajaran2;
+function setPelajaran2(Biologi){
+    pelajaran2 = Biologi
+}
+function getPelajaran2(Biologi){
+    return pelajaran2
+}
+console.log(getPelajaran2('Biologi'));
+
+//17
+let Agama1;
+function setAgama1(Budha){
+    Agama1 = Budha
+}
+function getAgama1 (Budha){
+    return Agama1
+}
+console.log(getAgama1('Budha'));
+
+//18
+let Makanan1;
+function setMakanan1(Sate){
+    Makanan1 = Sate
+}
+function getMakanan1 (Sate){
+    return Makanan1
+}
+console.log(getMakanan1('Sate'));
+
+//19
+let Umur;
+function setUmur(Lima){
+    umur = Lima
+}
+function getUmur (Lima){
+    return Umur
+}
+console.log(getUmur('Lima'));
+
+//20
+let Umur1;
+function setUmur1(Tujuh){
+    umur1 = Tujuh
+}
+function getUmur1(Tujuh){
+    return Umur1
+}
+console.log(getUmur1('Tujuh'));
