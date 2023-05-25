e1195a7eabed6cffa3f1de7f6bef958c67942b76
mida2709 <126259059+mida2709@users.noreply.github.com>
2023-05-25 09:19:00 +0700

    Step2.jsx

--- Productzilla_reactjs_form/src/components/Step2.jsx
+++ Productzilla_reactjs_form/src/components/Step2.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FormContext } from '../context/Form';
-import '../App.css';
 
 function Step2() {
   const navigate = useNavigate();
